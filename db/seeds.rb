4ba42dd9d21692ac6ba36678fd97adf73170ab85
Christian Schumacher <spam4@freenet.de>
2013-12-30 20:48:03 +0100

    Testfragen fuer die seed datei erstellt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create({"first_name"=>"Christian", "name"=>"Schumacher", "salutation"=>"Herr", "department_id"=>1, "position"=>"Tester", "password"=>12345, "email"=>"schumacherchristian85@googlemail.com","system_admin" =>1})
-Area.create({"name"=>"Test", "short_name"=>"test", "description"=>"Diese Abteilung dient den Systemadmins zum Testen nur Funktionen.", "area_admin_id"=>1})
+User.create({"first_name"=>"Christian", "name"=>"Schumacher", "salutation"=>"Dipl-Ing.", "department_id"=>1, "position"=>"Tester", "password"=>12345, "email"=>"schumacherchristian85@googlemail.com","system_admin" =>1})
+
+Area.create({"name"=>"Systemfragen", "short_name"=>"test", "description"=>"Dieser Fragenkatalog(Mathe) dient den Systemadmins zum Testen neuer Funktionen.", "area_admin_id"=>1})
+
+Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
