ec2f0f4056dc927a5e3bce8d8065d1d098d60dfc
Christian Schumacher <spam4@freenet.de>
2014-01-06 19:54:51 +0100

    seed angepasst

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@ User.create({"first_name"=>"Christian", "name"=>"Schumacher", "salutation"=>"Dip
 
 Area.create({"name"=>"Mathematik", "short_name"=>"mathe", "description"=>"Dieser Fragenkatalog(Mathe) dient den Systemadmins zum Testen neuer Funktionen.", "area_admin_id"=>1})
 
-Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "16 / 8", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "16-2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "4-4", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
-Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "solution"=> "01000", "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0, "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "16 / 8", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0, "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "16-2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0, "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "4-4", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0,  "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0,  "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0,  "author_id" => 1, "area_id" => 1 })
+Question.create({"question" => "2+2", "answer_1" => "2", "answer_2" => "4", "answer_3" => "11", "answer_4" => "2.5", "answer_5" => "16", "correct_1"=> 0, "correct_2"=> 1, "correct_3"=> 0, "correct_4"=> 0, "correct_5"=> 0,  "author_id" => 1, "area_id" => 1 })
 
 Navitem.create("name" => "Fragenkataloge","url" => "areas", "rang" => 1)
 Navitem.create("name" => "Fragen","url" => "questions", "rang" => 2)
